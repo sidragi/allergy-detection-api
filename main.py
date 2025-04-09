b33597a8990549b4072b317d9631a4e944482545
Sid Lahiri <sidlahiri04@gmail.com>
2025-04-09 17:18:35 +0530

    another /posy_url endppoint is added to handle images sent via url, this is specafically for botpress and it hosts the image on a public url, also requests is added to handle https requets

--- main.py
+++ main.py
@@ -57,3 +57,40 @@ async def predict(file: UploadFile = File(...)): #takes file input
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
+    
+class ImageURL(BaseModel):
+  url: str
+
+@app.post("/predict_url")
+async def predict_url(data: ImageURL):
+    try:
+        response = requests.get(data.url)
+        if response.status_code != 200:
+            raise HTTPException(status_code=400, detail="Failed to fetch image from URL")
+
+        image_bytes = np.asarray(bytearray(response.content), dtype=np.uint8)
+        img = cv2.imdecode(image_bytes, cv2.IMREAD_COLOR)
+
+        if img is None:
+            raise HTTPException(status_code=400, detail="Image decoding failed")
+
+        resize = tf.image.resize(img, (256, 256))
+        sev = float(model.predict(np.expand_dims(resize / 255.0, 0))[0][0])
+
+        if sev < 0.1:
+            result = "Severe allergy. Please visit a doctor immediately."
+        elif sev < 0.3:
+            result = "Moderate allergy. Please consult a doctor."
+        elif sev < 0.5:
+            result = "Mild allergy. Monitor the condition."
+        elif sev < 0.7:
+            result = "Low chance of allergy. Keep an eye on symptoms."
+        else:
+            result = "No signs of allergy."
+
+        percentage = round((1 - sev) * 100, 2)
+
+        return {"result": result, "severity": float(1-sev), "severity percentage": percentage}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"URL Prediction failed: {str(e)}")
